60a01dbdfad572f5894fae0c6b57eb4ef37ef1f5
Ryan Bachman <43930050+5K-AN00BIS@users.noreply.github.com>
2024-05-26 14:56:02 -0700

    Finishing touches are done on the website. Downloads now go to the users download folder and audiobook functions are working as expected.

--- app.py
+++ app.py
@@ -4,35 +4,34 @@
 # Summer 2024, Grad 1
 # Assignment 2.1 - Building Your Own eBook and Audiobook Web Application
 
-from flask import Flask, render_template, send_file, request, session, redirect, url_for
-
-import book_database
-from book_database import data, BOOKS, get_books, get_specific_book, get_book_details, db_session, Book, BookDetails
+from flask import Flask, render_template
+from book_database import get_books, get_specific_book, get_book_details, db_session, Book, BookDetails
+from pathlib import Path
 import pyttsx3
 
 
 app = Flask(__name__)
 
 
-def get_headers(response):
+def get_headers(response): # Get the headers from the response of the website.
     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '0'
 
 
-@app.route('/')
+@app.route('/')  # Display the home page so the user can start interacting with the website.
 def main():
     """Entry point; the view for the main page"""
     return render_template('index.html')
 
 
-@app.route('/second')
+@app.route('/second') # View the page showing
 def reading_page():
     """Views for the book details that a user can pick to read from the database."""
-    try:
-        books = get_books()
+    try:  # Attempt to do the following, otherwise...
+        books = get_books()  # Run the get_books function from the book_database.py file to get all of the book content to display.
         return render_template('Reading.html', books=books)
-    except Exception as e:
+    except Exception as e:  # Print out the exception page with a message.
         error_text = "<p>The error:<br>" + str(e) + "</p>"
         hed = '<h1>Something is broken.</h1>'
         return hed + error_text
@@ -41,39 +40,79 @@ def reading_page():
 @app.route('/second/<book_id>')
 def ereader_page(book_id):
     """Views for the book details that a user can pick to read from the database."""
-    try:
-        books = get_specific_book(book_id)
+    try:  # Attempt to do the following, otherwise...
+        books = get_specific_book(book_id)  # Take book_id as an argument to only output the specific book name and text the user chose.
         book_details = get_book_details(book_id)
 
-        text_to_speech("Hello, my name is Ryan!", "Male")
-
         return render_template('eReader.html', books=books, book_details=book_details)
-    except Exception as e:
+    except Exception as e:  # Print out the exception page with a message.
         error_text = "<p>The error:<br>" + str(e) + "</p>"
         hed = '<h1>Something is broken.</h1>'
         return hed + error_text
 
 
-@app.route('/third')
+@app.route('/download/<book_id>')
+def download_book(book_id):
+    try:  # Attempt to do the following, otherwise...
+        # Get the book name and text from the specific book_id that the user chose.
+        book_name = str(db_session.query(Book).filter_by(book_id=book_id).first().book_name)
+        book_text = str(db_session.query(BookDetails).filter_by(book_id=book_id).first().book_text)
+
+        # Set the download path to the users downloads folder.
+        downloads_path = str(Path.home() / 'Downloads')
+
+        # Write the book name and text to the users download folder with their book name as the file name.
+        f = open(downloads_path + "/" + book_name + ".txt", "w")
+        f.write(book_name)
+        f.write("\n\n")
+        f.write(book_text)
+        f.close()
+    except Exception as e:  # Print out the exception page with a message.
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
+
+    return render_template('download.html')
+
+
+@app.route('/audiobook/<book_id>')
+def audio_start(book_id):
+    try:  # Attempt to do the following, otherwise...
+        # Get the book name and text from the specific book_id that the user chose.
+        book_name = str(db_session.query(Book).filter_by(book_id=book_id).first().book_name)
+        book_text = str(db_session.query(BookDetails).filter_by(book_id=book_id).first().book_text)
+
+        # Read the books name and books text using text to speech so the user can hear instead of reading.
+        text_to_speech(book_name, "Female")
+        text_to_speech(book_text, "Female")
+    except Exception as e:  # Print out the exception page with a message.
+        error_text = "<p>The error:<br>" + str(e) + "</p>"
+        hed = '<h1>Something is broken.</h1>'
+        return hed + error_text
+
+    return render_template('audiobook.html')
+
+
+@app.route('/third')  # Set up the references page to see the resources I used for this assignment.
 def reference_page():
     """Views for the city details"""
     return render_template('References.html')
 
 
-def text_to_speech(text, gender):
+def text_to_speech(text, gender):  # Create the function that will be handling the text to speech for the audiobook.
     """
     Function to convert text to speech
     :param text: text
     :param gender: gender
     :return: None
     """
-    voice_dict = {'Male': 0, 'Female': 1}
-    code = voice_dict[gender]
+    voice_dict = {'Male': 0, 'Female': 1}  # Set the parameters for the male and female voice.
+    code = voice_dict[gender]  # Set the gender based on the function argument.
 
-    engine = pyttsx3.init()
+    engine = pyttsx3.init()  # Initiate the text to speech engine.
 
     # Setting up voice rate
-    engine.setProperty('rate', 125)
+    engine.setProperty('rate', 160)
 
     # Setting up volume level  between 0 and 1
     engine.setProperty('volume', 0.8)
@@ -82,9 +121,9 @@ def text_to_speech(text, gender):
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[code].id)
 
-    engine.say(text)
+    engine.say(text)  # Output the text to speech to the user.
     engine.runAndWait()
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  # Main function.
     app.run()
